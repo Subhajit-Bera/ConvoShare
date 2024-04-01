b5c7a3cff82dcdcc32bb326ed7772ccde930a073
Subhajit <subhabera211@gmail.com>
2024-04-01 19:30:54 +0530

    Login --

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+    <Container
+      component={"main"}
+      maxWidth="xs"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+
+
+
+      </Paper>
+
+
+    </Container>
   )
 }
 
