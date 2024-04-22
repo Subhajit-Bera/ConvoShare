1c2f3d50ff80f6400b6e52bcd4a4f32ea3eaf9dd
Subhajit <subhabera211@gmail.com>
2024-04-22 13:40:19 +0530

    Header.jsx -search,newgroup,notification ----2

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React, { Suspense, lazy,useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { orange } from "../../constants/color";
 import {
@@ -21,13 +21,16 @@ import {
   Notifications as NotificationsIcon,
 } from "@mui/icons-material";
 
+const SearchDialog = lazy(() => import("../specific/Search"));
+const NotifcationDialog = lazy(() => import("../specific/Notifications"));
+const NewGroupDialog = lazy(() => import("../specific/NewGroup"));
 
 
 const Header = () => {
   const [isMobile, setIsMobile] = useState(false);
   const [isSearch, setIsSearch] = useState(false);
   const [isNewGroup, setIsNewGroup] = useState(false);
-  const [isNotificatio, setIsNotification] = useState(false);
+  const [isNotification, setIsNotification] = useState(false);
 
   const navigate = useNavigate();
 
@@ -49,72 +52,99 @@ const Header = () => {
   }
   const navigateToGroup = () => navigate("/groups");
   return (
-    <Box sx={{ flexGrow: 1 }} height={"4rem"}>
-      <AppBar
-        position="static"
-        sx={{
-          bgcolor: orange,
-        }}
-      >
-        <Toolbar>
-          <Typography
-            variant="h6"
-            sx={{
-              display: { xs: "none", sm: "block" },
-            }}
-          >
-            ConvoShare
-          </Typography>
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar
+          position="static"
+          sx={{
+            bgcolor: orange,
+          }}
+        >
+          <Toolbar>
+            <Typography
+              variant="h6"
+              sx={{
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              ConvoShare
+            </Typography>
 
-          <Box
-            sx={{
-              display: { xs: "block", sm: "none" },
-            }}
-          >
-            <IconButton color="inherit" onClick={handleMobile}>
-              <MenuIcon />
-            </IconButton>
-          </Box>
-          <Box
-            sx={{
-              flexGrow: 1,
-            }}
-          />
-          <Box>
-            <IconBtn
-              title={"Search"}
-              icon={<SearchIcon />}
-              onClick={openSearch}
+            <Box
+              sx={{
+                display: { xs: "block", sm: "none" },
+              }}
+            >
+              <IconButton color="inherit" onClick={handleMobile}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Box
+              sx={{
+                flexGrow: 1,
+              }}
             />
+            <Box>
+              <IconBtn
+                title={"Search"}
+                icon={<SearchIcon />}
+                onClick={openSearch}
+              />
 
-            <IconBtn
-              title={"New Group"}
-              icon={<AddIcon />}
-              onClick={openNewGroup}
-            />
+              <IconBtn
+                title={"New Group"}
+                icon={<AddIcon />}
+                onClick={openNewGroup}
+              />
 
-            <IconBtn
-              title={"Manage Groups"}
-              icon={<GroupIcon />}
-              onClick={navigateToGroup}
-            />
+              <IconBtn
+                title={"Manage Groups"}
+                icon={<GroupIcon />}
+                onClick={navigateToGroup}
+              />
 
-            <IconBtn
-              title={"Notifications"}
-              icon={<NotificationsIcon />}
-              onClick={openNotification}
+              <IconBtn
+                title={"Notifications"}
+                icon={<NotificationsIcon />}
+                onClick={openNotification}
               // value={notificationCount}
-            />
+              />
 
-            <IconBtn
-              title={"Logout"}
-              icon={<LogoutIcon />}
-              onClick={logoutHandler}
-            />
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+              <IconBtn
+                title={"Logout"}
+                icon={<LogoutIcon />}
+                onClick={logoutHandler}
+              />
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+
+      {
+        isSearch && (
+          <Suspense fallback={<div>Loading..</div>}>
+            <SearchDialog />
+          </Suspense>
+        )
+      }
+
+      {
+        isNotification && (
+          <Suspense fallback={<div>Loading..</div>}>
+            <NotifcationDialog />
+          </Suspense>
+        )
+      }
+
+      {
+        isNewGroup && (
+          <Suspense fallback={<div>Loading..</div>}>
+            <NewGroupDialog />
+          </Suspense>
+        )
+      }
+    </>
   )
 }
 
